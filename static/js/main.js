976a4db01ee4286746008cd1563a7dcf6fa08b98
Buddy Lindsey <buddy@buddylindsey.com>
2013-04-04 21:03:30 -0500

    fix index page and add active mneu items

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,20 @@
 $(document).ready(function() {
+
+  if(window.location.pathname === "/about/"){
+    $(".nav-about").addClass("active");
+  }
+
+  switch (window.location.pathname) {
+    case "/":
+      $(".nav-home").addClass("active");
+      break;
+    case "/about/":
+      $(".nav-about").addClass("active");
+      break;
+    case "/feedback/":
+      $(".nav-feedback").addClass("active");
+      break;
+  }
+
   $('#content-tabs a:first').tab('show');
 });
