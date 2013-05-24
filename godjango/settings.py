58c175471019ba7ecad186d3ae832c59b1e2a294
Buddy Lindsey <buddy@buddylindsey.com>
2013-05-23 20:06:40 -0500

    Changes for better production

--- godjango/settings.py
+++ godjango/settings.py
@@ -28,6 +28,8 @@ else:
 
 TEMPLATE_DEBUG = DEBUG
 
+ALLOWED_HOSTS = os.environ['ALLOWED_HOSTS']
+
 ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 path = lambda *a: os.path.join(ROOT, *a)
 
@@ -75,7 +77,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = "%s/static/" % path('.')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -86,7 +88,6 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    "%s/static" % path('.'),
 )
 
 # List of finder classes that know how to find static files in
