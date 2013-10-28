d4b29818960906b07301522d7b160657802aa878
Buddy Lindsey <buddy@buddylindsey.com>
2013-10-28 13:15:10 -0500

    Add update card functionality

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,29 +1,22 @@
 from django.conf.urls import *
-from django.views.generic import TemplateView
 from django.views.generic.base import RedirectView
-from django.contrib.auth import logout as auth_logout
 from django.contrib.auth.decorators import login_required
 
-from accounts.views import BillingView, SettingsView, DashboardView
+from accounts.views import (
+    BillingView, SettingsView, DashboardView, UpdateBillingView)
 
 
-urlpatterns = patterns('accounts.views',
+urlpatterns = patterns(
+    'accounts.views',
     url(r'', include('social_auth.urls')),
-    url(r'^dashboard/$', 
-        login_required(DashboardView.as_view()), 
-        name="dashboard"
-    ),
+    url(r'^dashboard/$',
+        login_required(DashboardView.as_view()), name="dashboard"),
     url(r'^favorites/$', 'favorites', name="favorites"),
-    url(r'^settings/$',
-        login_required(SettingsView.as_view()),
+    url(
+        r'^settings/$', login_required(SettingsView.as_view()),
         name="settings"),
-    url(r'^billing/$',
-        login_required(BillingView.as_view()),
-        name="billing"
-    ),
-    url(r'^login/$',
-        RedirectView.as_view(url="/accounts/login/github")
-    ),
+    url(r'^billing/$', login_required(BillingView.as_view()), name="billing"),
+    url(r'^update_card/$', UpdateBillingView.as_view(), name='update_card'),
+    url(r'^login/$', RedirectView.as_view(url="/accounts/login/github")),
     url(r'^logout/$', 'logout', name='logout'),
-
 )
