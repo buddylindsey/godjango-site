5d285fbdb1352964d5d1e8567ef90c6ac86d4448
Buddy Lindsey <buddy@buddylindsey.com>
2014-07-03 23:39:10 -0500

    add chart to admin

--- analytics/views.py
+++ analytics/views.py
@@ -1,6 +1,9 @@
 from datetime import datetime, timedelta
 
 from django.views.generic import TemplateView
+from django.contrib.auth.models import User
+
+import arrow
 
 from payments.models import CurrentSubscription
 
@@ -12,12 +15,26 @@ class AnalyticsIndexView(TemplateView):
         context = super(AnalyticsIndexView, self).get_context_data(**kwargs)
         context['active_subscribers'] = self.active_subscribers()
         context['30_day_new'] = self.thirty_day_new()
+        context['30_day_registrations'] = self.thirty_day_registrations()
         return context
 
     def active_subscribers(self):
-        return CurrentSubscription.objects.filter(status='active').count() - 6
+        return CurrentSubscription.objects.filter(status='active').count() - 5
 
     def thirty_day_new(self):
         prev_date = datetime.now() - timedelta(days=30)
         return CurrentSubscription.objects.filter(
             start__gte=prev_date, status='active').count()
+
+    def thirty_day_registrations(self):
+        final_data = []
+
+        date = arrow.now()
+        for day in xrange(1, 30):
+            date = date.replace(days=-1)
+            count = User.objects.filter(
+                date_joined__gte=date.floor('day').datetime,
+                date_joined__lte=date.ceil('day').datetime).count()
+            final_data.append(count)
+
+        return final_data
