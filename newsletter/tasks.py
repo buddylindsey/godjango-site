5d0ffb5de0029e19ccf85cf68c89934da7fa7231
Buddy Lindsey Jr <buddy@buddylindsey.com>
2015-06-28 16:18:29 -0500

    try to avoid duplicating email when subscribing to mailchimp

--- newsletter/tasks.py
+++ newsletter/tasks.py
@@ -4,9 +4,14 @@ from django.conf import settings
 
 from godjango.celery import app
 
+from .models import
+
 
 @app.task
 def newsletter_subscribe(first_name, last_name, email):
+    if Subscriber.objects.filter(email=email).exists():
+        return
+
     try:
         mc = mailchimp.Mailchimp(settings.MAILCHIMP_API_KEY)
 
