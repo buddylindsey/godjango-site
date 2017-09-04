4b3a130425eccf516fb073905f113cc8294fd86b
Buddy Lindsey, Jr <buddylindsey@users.noreply.github.com>
2017-09-04 14:48:25 -0500

    Remove newsletter subscribe

--- apps/newsletter/mixins.py
+++ apps/newsletter/mixins.py
@@ -3,4 +3,4 @@ from .tasks import newsletter_subscribe
 
 class MailchimpMixin(object):
     def newsletter_subscribe(self, first_name, last_name, email, tags=None):
-        newsletter_subscribe.delay(first_name, last_name, email, tags)
+        pass
